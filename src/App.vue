2465bc0cd6fffa0fb4e78e7b27598069367d1118
kagg886 <iveour@163.com>
2023-11-07 11:17:36 +0800

    feat: 🎉 init page

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import {ref} from "vue";
+
+const a = ref<string>()
+utools.onPluginEnter((action) => {
+  a.value = JSON.stringify(action);
+})
+</script>
+
+<template>
+  {{a}}
+</template>
+
+<style scoped>
+
+</style>
